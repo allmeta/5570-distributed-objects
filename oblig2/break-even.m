d062b0125df5a24f56c51ddef97745577ce817ad
allmeta <thomasdasd@gmail.com>
2020-03-10 19:40:44 +0100

    kys irl sync done ish

---
+++ oblig2/break-even.m
@@ -0,0 +1,16 @@
+ X.f[visit b]
+
+emulated by:
+
+   home <- locate self
+
+   move b to X
+
+   X.f[b, home]
+   op f[arg: T, home: Node]
+
+      ...
+
+      move arg to home
+
+   end f
--- oblig2/time-sync.m
+++ oblig2/time-sync.m
@@ -1,25 +1,27 @@
 const master <- object master
+  const home<-locate self
+  var nodes: NodeList<-home.getActiveNodes
   process
-    const home<-locate self
-    var nodes: NodeList<-home.getActiveNodes
     loop
-      sync
-      home.delay[Time.create[60,0]]
+      begin
+        stdout.putString[(nodes.upperbound + 1).asString || " nodes active.\n"]
+        for e in nodes
+          const reqStartTime<- home$timeofday
+          const time<- e$thenode$timeofday
+          const reqEndTime<-home$timeofday
+          const rtt <- reqEndTime-reqStartTime
+          const est<-time+rtt/2
+          stdout.putString["name:\t"||e$thenode$name||"\n"]
+          stdout.putString["est:\t"||est.asstring||"\n"]
+          stdout.putString["Rtt:\t"||rtt.asstring||"\n"]
+          stdout.putString["Diff:\t"||(reqStartTime-est).asString||"\n"]
+          stdout.putString["--------\n"]
+        end for
+        home.delay[Time.create[60,0]]
+        unavailable
+          stdout.putString["Unavailable...\n"]
+        end unavailable
+      end
     end loop
   end process
-  export function sync 
-    stdout.putString[(nodes.upperbound + 1).asString || " nodes active.\n"]
-    for e in nodes
-      var now: Time<-home.getTimeOfDay
-      var time: Time<-e.getTimeOfDay
-      var rtt: Time<-time-now
-      var est:Time<-cristian[time,rtt]
-      stdout.putString[e.name||"\n"]
-      stdout.putString[est.asstring||"\n"]
-      stdout.putString["--------\n"]
-    end for
-  end sync
-  export function cristian [time:Time,rtt:Time]->[res:Time]
-    res<-time+rtt/2
-  end cristian
 end master
