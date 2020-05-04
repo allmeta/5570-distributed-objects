268fbb977c9ef28df76cac53b6aadbb0aeba38bc
allmeta <thomasdasd@gmail.com>
2020-05-04 16:59:24 +0200

    home2

---
+++ home2/framework.m
@@ -0,0 +1,5 @@
+const framework <- object framework
+  const index_objectid_nodes <- Directory.empty
+  const index_objectid_master <- Directory.empty
+  const index_node <- Directory.empty
+end framework
