6c6d67ca7ff025996c3c673ea94d457c27aa16c7
allmeta <thomasdasd@gmail.com>
2020-05-22 06:01:19 +0200

    tests done

--- home2/test.m
+++ home2/test.m
@@ -4,8 +4,31 @@ const test <- object test
   const so <- here$stdout
   initially
     here.delay[Time.create[1,0]]
-    self.start
+    % self.start
+    % self.uptest
+    self.downtest
   end initially
+  export op downtest
+    const x<-testObject.create["x1"]
+    const y<-testObject.create["y1"]
+    const x_replicas<-framework.replicateMe[x,2]
+    const y_replicas<-framework.replicateMe[y,1]
+    framework.dump
+    so.putstring["node down test"]
+    here.delay[Time.create[10,0]]
+    framework.dump
+    
+  end downtest
+
+  export op uptest
+    so.putstring["node up test"]
+    here.delay[Time.create[10,0]]
+    const x<-testObject.create["x1"]
+    const y<-testObject.create["y1"]
+    const x_replicas<-framework.replicateMe[x,4]
+    const y_replicas<-framework.replicateMe[y,1]
+    framework.dump
+  end uptest
   export op start
     % name server
     so.putstring["name server tests\n\n"]
@@ -26,19 +49,19 @@ const test <- object test
     % time server
     so.putstring["time server test\n\n"]
     self.calctime[framework.getObject["y1"]]
-    self.calctime[framework.getObject["x2"]]
+    self.calctime[framework.getObject["x1"]]
 
   end start
 
   export op calctime[ob:testObjectType]
     const reqStartTime<-here$timeofday
-    const time<- ob$time
+    const t<- ob$time
     const reqEndTime<-here$timeofday
     const rtt <- reqEndTime-reqStartTime
-    const est<-time+rtt/2
+    const est<-t+rtt/2
     here$stdout.putString["NAME:\t"||ob$name||"\n"]
-    here$stdout.putString["ESTIMATE:\t"||est.asstring||"\n"]
     here$stdout.putString["RTT:\t"||rtt.asstring||"\n"]
+    here$stdout.putString["ESTIMATE:\t"||est.asstring||"\n"]
     here$stdout.putString["DIFF:\t"||(est-reqEndTime).asString||"\n"]
     here$stdout.putString["--------\n"]
   end calctime
